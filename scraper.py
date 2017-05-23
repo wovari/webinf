9bdb5fc4853f565bd41fc253efa1c2ddb12e7099
Wout Van Riel <wvriel@infogroep.be>
2017-05-23 21:48:54 +0200

    Code commented

--- scraper.py
+++ scraper.py
@@ -4,18 +4,35 @@ from actor import Actor
 from movie import Movie
 from collections import deque
 
+"""
+Actor and Movie entities are saved in a dictionary with key the URL of their page on IMDB.
+The translate dictionary is used to quickly translate an actors name to a valid URL.
+    actor_dict -- dictionary contains Actor classes and has as key the URL of the actors page on IMDB
+    movie_dict -- dictionary contains Movie classes and has as key the URL of the movie page on IMDB
+    translate_actor_to_url -- dictionary contains the name of the actor as key and the URL of their page as value
+"""
 actor_dict = {}
-
 movie_dict = {}
-
 translate_actor_to_url = {}
 
-actors_scraped = set()
-
-processed_actors = set()
-
+"""
+The sets are used to keep track of all the actors already encountered.
+When an actors URL is added to the queue, it is also added to actors_encountered.
+This makes it easy during scraping to detect actors that are already scraped or waiting in the queue.
+    actors_encountered -- set containing urls of actors
+    processing_queu -- deque where all actors that are encountered but not yet processed are added to
+"""
+actors_encountered = set()
 processing_queue = deque()
 
+"""
+IMDB_scraper is the function that creates a graph from a given actor as root with a degree K.
+The nodes (actors) are stored in actor_dict and the edges are stored in the Actor class as connections.
+    actor-name -- Name of the actor that will be used as root
+    degree -- Find all actors till that degree of the actor
+    thr_mov -- Limit on how many movies are selected
+    thr_act -- Limit on how many actors are selected 
+"""
 def IMDB_scraper(actor_name, degree, thr_mov, thr_act):
     root_actor = find_actor(actor_name)
     add_to_queue(root_actor.url)
@@ -28,7 +45,11 @@ def IMDB_scraper(actor_name, degree, thr_mov, thr_act):
         create_border(act, degree, thr_mov, thr_act)
 
 
-
+"""
+find_actor searches on IMDB for the actor with given name and returns an Actor object.
+The actor is searched with the search page of IMDB where the scraper selects the first actor in the returned page.
+    actor_name -- Name of the actor that will be searched.
+"""
 def find_actor(actor_name):
     search_url = "http://www.imdb.com/find?q=" + actor_name
     cast_page = requests.get(search_url)
@@ -41,33 +62,45 @@ def find_actor(actor_name):
     translate_actor_to_url[actor_name] = actor_url
     return act
 
-
-# return everything except TV Series
-def list_movies_of_actor(actor_url, threshold=float('inf')):
+"""
+list_movies_of_actor is a function that returns the list with movies where the actor played a role in.
+The limit parameter, if not infinite, will make sure that the length of the returned list is not longer than given limit.
+    actor_url -- URL of the actor
+    limit -- default value is infinite, limits the size of returned list
+"""
+def list_movies_of_actor(actor_url, limit=float('inf')):
     movie_lst = []
     cast_page = requests.get(actor_url)
     cast_tree = html.fromstring(cast_page.text)
     path_to_movies = cast_tree.xpath('//div[@class="filmo-category-section"]'
                                      '/div[not(text()[contains(., "TV Series")])]/b/a')
     for mov in path_to_movies:
-        if threshold <= 0:
+        if limit <= 0:
             break
         movie_name = mov.text
         movie_url = strip_url("http://www.imdb.com" + mov.attrib.get('href'))
         if not movie_url in movie_dict:
             movie_dict[movie_url] = Movie(movie_name, movie_url)
         movie_lst.append(movie_url)
-        threshold -= 1
+        limit -= 1
     return movie_lst
 
-
-def list_cast_of_movie(movie_url, threshold=float('Inf'), cast_lookup=True, add_new_actors=True):
+"""
+list_cast_of_movie returns the list of the cast that acted in the movie. 
+The limit parameter, if not infinite, will make sure that the length of the returned list is not longer than given limit.
+    movie_url -- URL of the movie
+    limit -- default value is infinite, limits the size of returned list
+    cast_lookup -- default value is True, if False the function will not look in the dictionary to see if the movie
+                   is already scraped.
+    add_new_actors -- default value is True, if False the function will not add actors to the list that not yet encountered
+"""
+def list_cast_of_movie(movie_url, limit=float('Inf'), cast_lookup=True, add_new_actors=True):
     mov = movie_dict[movie_url]
-    if len(mov.cast) > 0 and (threshold <= len(mov.cast) or threshold ==float('Inf')) and cast_lookup:
-        if threshold == float('Inf'):
+    if len(mov.cast) > 0 and (limit <= len(mov.cast) or limit ==float('Inf')) and cast_lookup:
+        if limit == float('Inf'):
             return mov.cast
         else:
-            return mov.cast[:threshold]
+            return mov.cast[:limit]
     else:
         actor_lst = []
         cast_url = movie_url + "/fullcredits?ref_=tt_ov_st_sm"
@@ -77,7 +110,7 @@ def list_cast_of_movie(movie_url, threshold=float('Inf'), cast_lookup=True, add_
                                          '/tr[@class="odd" or @class="even"]'
                                          '/td[@class="itemprop"]/a')
         for act in path_to_actors:
-            if threshold <= 0:
+            if limit <= 0:
                 break
             actor_name = act[0].text
             actor_url = strip_url("http://www.imdb.com" + act.attrib.get('href'))
@@ -87,30 +120,49 @@ def list_cast_of_movie(movie_url, threshold=float('Inf'), cast_lookup=True, add_
                 translate_actor_to_url[actor_name] = actor_url
                 add_to_queue(actor_url)
             actor_lst.append(actor_url)
-            threshold-=1
+            limit-=1
         return actor_lst
 
 
+"""
+strip_url is a function that strips a URL string of unnecessary data and returns the result
+    url -- url that needs to be stripped
+"""
 def strip_url(url):
     url = url.split("?")[0]
     url = url[:-1]
     return url
 
-
+"""
+add_to_queue is a function that handles which actors are added to the queue. 
+It adds them only if they are not yet encountered.
+    actor_url -- URL of actor that is to be added to the queue
+"""
 def add_to_queue(actor_url):
-    if not actor_url in actors_scraped:
+    if not actor_url in actors_encountered:
         processing_queue.append(actor_url)
-        actors_scraped.add(actor_url)
+        actors_encountered.add(actor_url)
 
-
+"""
+create_edges searches all actors where the given actor has worked with. 
+Every connection (the movie and the co-actor) are added to the actors Actor object connection dictionary.
+Only the first connection with a certain co-actor is saved. If later a movie is encountered in which the actor
+also played with the co-actor, the connection is skipped.
+The co-actor also adds the actor to its connections.
+    actor_url -- URL of actor
+    degree -- degree of root actor (to be saved in the actors object)
+    thr_mov -- limit for the movie selection
+    thr_act -- limit for the actor selection
+"""
 def create_edges(actor_url,degree, thr_mov, thr_act):
-    mov_lst = list_movies_of_actor(actor_url,threshold=thr_mov)
+    mov_lst = list_movies_of_actor(actor_url, limit=thr_mov)
     unique_actors = set()
     actor = actor_dict[actor_url]
+    unique_actors |= set(list(actor.connections.keys()))
     print("Getting edges for: "+actor.name)
     actor.degree = degree
     for m in mov_lst:
-        act_lst = list_cast_of_movie(m, threshold=thr_act)
+        act_lst = list_cast_of_movie(m, limit=thr_act)
         for a in act_lst:
             if not a in unique_actors and a != actor_url:
                 actor.add_connection(a, m)
@@ -121,24 +173,37 @@ def create_edges(actor_url,degree, thr_mov, thr_act):
                         actor_dict[a] = tmp_act
                 unique_actors.add(a)
     actor_dict[actor_url] = actor
-    processed_actors.add(actor_url)
 
+"""
+create_border searches all actors where the given actor has worked with and that are already encountered.
+This function is used to connect the actor with actors that are already present in the graph. 
+For example if we want to end the graph at the 3rd degree, the actors on this degree can still connect 
+with eachother without adding new actors to the queue.
+    actor_url -- URL of actor
+    degree -- degree of root actor (to be saved in the actors object)
+    thr_mov -- limit for the movie selection
+    thr_act -- limit for the actor selection    
+"""
 def create_border(actor_url, degree, thr_mov, thr_act):
-    mov_lst = list_movies_of_actor(actor_url, threshold=thr_mov)
+    mov_lst = list_movies_of_actor(actor_url, limit=thr_mov)
     unique_actors = set()
     actor = actor_dict[actor_url]
     print("Getting edges for: "+actor.name)
     actor.degree = degree
     for m in mov_lst:
-        act_lst = list_cast_of_movie(m, threshold=thr_act, add_new_actors=False)
+        act_lst = list_cast_of_movie(m, limit=thr_act, add_new_actors=False)
         for a in act_lst:
-            if not a in unique_actors and a in actors_scraped and a != actor_url:
+            if not a in unique_actors and a in actors_encountered and a != actor_url:
                 actor.add_connection(a, m)
                 unique_actors.add(a)
     actor_dict[actor_url] = actor
-    processed_actors.add(actor_url)
 
-
+"""
+find_shortest_path is a recursive function that tries to find the shortest path between two vertices. 
+    start -- URL of actor from where to start
+    end -- URL of actor that needs to be found
+    path -- the current path
+"""
 def find_shortest_path(start, end, path=[]):
     path = path + [start]
     if start == end:
@@ -150,10 +215,12 @@ def find_shortest_path(start, end, path=[]):
             if newpath:
                 if not shortest or len(newpath) < len(shortest):
                     shortest = newpath
-
-
     return shortest
 
+"""
+shortest_path is a function that calls find_shortest_path and translates the returned list of URLs to readable connections.
+    start -- URL of actor from where to start
+    end -- URL of actor that needs to be found"""
 def shortest_path(start, end):
     shortest = find_shortest_path(start, end)
     return_string = ''
@@ -165,13 +232,6 @@ def shortest_path(start, end):
         return_string += act.name + '--('+mov.title+')-->' + act_to.name + '\n'
         act = act_to
     return return_string
-
-def print_connections(actor_url):
-    actor = actor_dict[actor_url]
-    for act in actor.connections:
-        act_name = actor_dict[act].name
-        mov_name = movie_dict[actor.connections[act]].title
-        print(act_name+" : "+mov_name)
 
 
 if __name__ == "__main__":
